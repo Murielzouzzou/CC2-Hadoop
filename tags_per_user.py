2b4a50947772dc4e1341c00167f25378a012f03a
mzouzzou <159195249+Murielzouzzou@users.noreply.github.com>
2026-04-09 16:38:04 +0200

    Add files via upload
    Projet Hadoop MapReduce : analyse des tags du dataset MovieLens (tags par film, utilisateur et fréquence des tags)

---
+++ tags_per_user.py
@@ -0,0 +1,31 @@
+from mrjob.job import MRJob                                                               
+                                                                                          
+                                                                                          
+                                                                                          
+class TagsPerUser(MRJob):                                                                 
+                                                                                          
+                                                                                          
+                                                                                          
+    def mapper(self, _, line):                                                            
+                                                                                          
+        try:                                                                              
+                                                                                          
+            userId, movieId, tag, timestamp = line.split(',')                             
+                                                                                          
+            yield userId, 1                                                               
+                                                                                          
+        except:                                                                           
+                                                                                          
+            pass                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+    def reducer(self, key, values):                                                       
+                                                                                          
+        yield key, sum(values)                                                            
+                                                                                          
+                                                                                          
+                                                                                          
+if __name__ == '__main__':                                                                
+                                                                                          
+    TagsPerUser.run()   
